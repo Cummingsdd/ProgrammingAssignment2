5bf85799d297803aa3003739183ac0665b25dd5d
cummingsdd <cummings.dd@gmail.com>
2015-02-15 21:09:53 -0500

    Core functionality complete

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <- NULL
+    set <- function(y) {
+        x <<- y
+        cache <<- NULL
+    }
+    get <- function() x
+    set_result <- function(val) cache <<- val
+    get_result <- function() cache
+    get_function <- function() fun
+    list(set = set, get = get, 
+         set_result = set_result,
+         get_result = get_result)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$get_result()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$set_result(m)
+    m
 }
