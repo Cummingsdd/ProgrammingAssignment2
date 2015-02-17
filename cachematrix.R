82baa6cf5bf572af6ce611325c30d44d2b1d9527
cummingsdd <cummings.dd@gmail.com>
2015-02-16 19:05:16 -0500

    Comments updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions provide a mechanism to store a matrix and the result of a (presumably) time 
+## consuming expression on that matrix in a single data structure. 
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function will take a matrix and encapsulate it in a list
+## that consists of the original matrix, an empty result, and functions to get 
+## and set these two.
 
 makeCacheMatrix <- function(x = matrix()) {
-    cache <- NULL
+    #initilize the cached result
+    cache <- NULL 
+    #setter for the input value
     set <- function(y) {
         x <<- y
         cache <<- NULL
     }
+    #getter for the input value
     get <- function() x
+    #setter for function result
     set_result <- function(val) cache <<- val
+    #getter for result
     get_result <- function() cache
-    get_function <- function() fun
+    # List object storing the setter and getter functions
     list(set = set, get = get, 
          set_result = set_result,
          get_result = get_result)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a "makeCacheMatrix" list, x, containing an invertible matrix and parameters for the 
+##"solve" function. If the inverse has been calculated it will be returned, else it will be found, stored
+## in x and the returned.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'.
